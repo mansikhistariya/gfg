e97221389429ee8d5c68d272f1e13623d57a5e0e
mansi khistariya <119156055+mansikhistariya@users.noreply.github.com>
2024-01-15 12:17:47 +0530

    Added Solution - GfG to GitHub

---
+++ School/Palindromic Array/palindromic-array.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.util.*;
+class PalindromicArray{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+			int n = sc.nextInt();
+			int[] a = new int[n];
+			for(int i = 0 ;i < n; i++)
+				a[i]=sc.nextInt();
+			GfG g = new GfG();
+			System.out.println(g.palinArray(a , n));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the Function below*/
+class GfG {
+    public static int palinArray(int[] a, int n) {
+        for (int i = 0; i < n; i++) {
+            int reverse = rev(a[i]);
+            if (reverse != a[i]) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    private static int rev(int num) {
+        int reverse = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+        return reverse;
+    }
+}
