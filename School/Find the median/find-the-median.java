cb1481dbd957edf6356107db08e6b7895f110c8a
mansi khistariya <119156055+mansikhistariya@users.noreply.github.com>
2024-01-18 20:18:19 +0530

    Added Solution - GfG to GitHub

---
+++ School/Find the median/find-the-median.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] v  = new int[n];
+            for(int  i = 0; i < n; i++)
+                v[i] = Integer.parseInt(S[i]);
+            Solution ob = new Solution();
+            int ans = ob.find_median(v);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int find_median(int[] v)
+    {
+        int median=0;
+       Arrays.sort(v);
+       if(v.length %2==0){
+           int n=(v.length-1)/2;
+           if(n<v.length){
+               median=(v[n]+v[n+1])/2;
+           }
+          
+       }
+       if(v.length %2!=0){
+           int n=(v.length-1)/2;
+           if(n<v.length){
+                median=v[n];
+           }
+          
+       }
+       return median;
+    }
+}
