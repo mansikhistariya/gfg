7338885b74ffa85cb241ffb2ddb4d6c746aa7807
mansi khistariya <119156055+mansikhistariya@users.noreply.github.com>
2024-01-21 18:44:46 +0530

    Added Solution - GfG to GitHub

---
+++ School/Upper case conversion/upper-case-conversion.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])throws IOException
+        {
+            BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+            int t = Integer.parseInt(br.readLine());
+            while(t-->0)
+                {
+                    String str = br.readLine();
+                    Solution ob = new Solution();
+                    System.out.println(ob.transform(str));
+                }
+        }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String transform(String s)
+    {
+        String newStr= "";
+        String[] strs = s.split(" ");
+        for(String str: strs){
+            newStr += str.substring(0,1).toUpperCase()+ str.substring(1)+" ";;
+        }
+        return newStr;
+    }
+}
