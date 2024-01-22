773a2e47f9e2167bd1f6c75cfdecf767e2ea02c2
mansi khistariya <119156055+mansikhistariya@users.noreply.github.com>
2024-01-22 16:32:02 +0530

    Added Solution - GfG to GitHub

---
+++ School/Java Strings | Set 1/java-strings-set-1.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S1 = read.readLine();
+            String S2 = read.readLine();
+            Solution ob = new Solution();
+
+            System.out.println(ob.conRevstr(S1, S2));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function template for Java
+
+class Solution {
+    static String conRevstr(String S1, String S2) {
+       String s3=S1+S2;
+       StringBuilder sb=new StringBuilder(s3);
+       sb.reverse();
+       return sb.toString();
+    }
+}
