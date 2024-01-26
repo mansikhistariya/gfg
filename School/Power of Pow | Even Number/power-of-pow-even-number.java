fb345f5eb983ac008fb3891234e5cca9b19e9c1e
mansi khistariya <119156055+mansikhistariya@users.noreply.github.com>
2024-01-26 12:07:30 +0530

    Added Solution - GfG to GitHub

---
+++ School/Power of Pow | Even Number/power-of-pow-even-number.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());            
+            Solution ob = new Solution();
+            long ans = ob.sum_of_square_evenNumbers(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long sum_of_square_evenNumbers(long n)
+    {
+       long sum=0;
+        int k=0;
+       for(int i=1;i<=n*2;i++){
+           if(i%2==0){
+              sum+=Math.pow(i,2);
+           }
+       }
+       return sum;
+    }
+}
