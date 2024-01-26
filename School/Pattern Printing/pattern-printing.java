6c83078e4afe855171246340ba5c7f452266b33b
mansi khistariya <119156055+mansikhistariya@users.noreply.github.com>
2024-01-26 15:04:06 +0530

    Added Solution - GfG to GitHub

---
+++ School/Pattern Printing/pattern-printing.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			
+            Solution ob = new Solution();
+            ob.printPattern(N);
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static void printPattern(int N){
+     for(int i=1;i<=N;i++){
+            String str="*";
+            System.out.print(str.repeat(i)+" ");
+        } 
+    }
+}
