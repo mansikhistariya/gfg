f6b4424efcd7bb5bafada7129821bec9378dc73c
mansi khistariya <119156055+mansikhistariya@users.noreply.github.com>
2024-02-01 12:14:35 +0530

    Added Solution - GfG to GitHub

---
+++ School/Number of Diagonals/number-of-diagonals.java
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int N = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.diagonals(N));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long diagonals(int n) 
+    { 
+       return (long)n*(n-3)/2;
+    } 
+}
