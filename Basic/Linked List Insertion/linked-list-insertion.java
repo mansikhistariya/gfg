872155ec40703d54ed638a666e9e59cc526e528e
mansi khistariya <119156055+mansikhistariya@users.noreply.github.com>
2024-02-06 10:55:33 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Linked List Insertion/linked-list-insertion.java
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+class GFG{
+    static void printList(Node node) 
+    { 
+        while (node != null) 
+        { 
+            System.out.print(node.data + " "); 
+            node = node.next; 
+        } 
+        System.out.println(); 
+    }
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+            int n = sc.nextInt();
+            Node head = null;
+            for(int i=0; i<n; i++)
+            {
+                int value = sc.nextInt();
+                int indicator = sc.nextInt();
+
+                Solution ob = new Solution();
+                if(indicator == 0)
+                    head = ob.insertAtBeginning(head, value);
+                else
+                    head = ob.insertAtEnd(head, value);
+            }
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to insert a node at the beginning of the linked list.
+    Node insertAtBeginning(Node head, int x)
+    {
+        Node newnode=new Node(x);
+        
+             newnode.next=head;
+            //   head=newnode;
+             
+        return newnode;
+    }
+    
+    //Function to insert a node at the end of the linked list.
+    Node insertAtEnd(Node head, int x)
+    {
+         Node newnode=new Node(x);
+        if(head==null){
+            return newnode;
+            
+        }else{
+       
+           Node currnode=head;
+             while(currnode.next!=null){
+                 currnode=currnode.next;
+             }
+         
+            currnode.next=newnode;
+        }
+       
+        return head;
+    
+    }
+}
