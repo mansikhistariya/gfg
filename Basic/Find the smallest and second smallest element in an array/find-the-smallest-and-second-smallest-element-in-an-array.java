387dc3491f29c6e284632b8ff2cb8d19f1a7cacc
mansi khistariya <119156055+mansikhistariya@users.noreply.github.com>
2024-02-15 10:42:39 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find the smallest and second smallest element in an array/find-the-smallest-and-second-smallest-element-in-an-array.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            long[] product = obj.minAnd2ndMin(a, n); 
+            if(product[0]==-1)
+                System.out.println(product[0]);
+            else
+                System.out.println(product[0]+" "+product[1]);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute 
+{
+    public long[] minAnd2ndMin(long a[], long n)  
+    {
+       Arrays.sort(a);
+        long result = 0;
+        
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i] != a[i + 1]) {
+                result = a[i + 1];
+                break;
+            }
+        }
+        
+        if (result == 0) {
+            return new long[]{-1};
+        } else {
+            long[] arr = {a[0], result};
+            return arr;
+        }
+    }
+}
