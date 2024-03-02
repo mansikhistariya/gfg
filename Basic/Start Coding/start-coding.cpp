edf989c04dbd42b3cd4765467836bf2d19048822
mansi khistariya <119156055+mansikhistariya@users.noreply.github.com>
2024-03-02 20:35:34 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Start Coding/start-coding.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<iostream>
+
+using namespace std;
+
+void printHello();
+
+// } Driver Code Ends
+//User function Template for C++
+
+// Function to print "Hello World"
+void printHello(){
+    
+    // Your code goes here
+     if(cout<<"Hello World"){
+        
+    }
+    
+}
+
+//{ Driver Code Starts.
+
+int main()
+{
+    // Calling function to print Hello World
+    printHello();
+
+return 0;
+}
+// } Driver Code Ends
